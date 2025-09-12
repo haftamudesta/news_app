097754531898aeb3ee4880fab9e7cf935f02af9d
haftamudesta <desta451616@hotmail.com>
2025-09-11 23:59:31 -0800

    Add rule for registration

---
+++ app/api/auth/actions/auth.js
@@ -0,0 +1,35 @@
+export async function register(state,formData){
+        const validateFormFields=RegisterFormSchema.safeParse({
+                email:formData.get("email"),
+                password:formData.get("password"),
+                confirmPassword:formData.get("confirmPassword")
+        })
+        if(!validateFormFields.success){
+                return{
+                        errors:validateFormFields.error.flatten().fieldErrors,
+                        email:formData.get("email"),
+                }
+        }
+
+        const {email,password}=validateFormFields.data
+        const userCollection=await getCollection("users");
+        if(!userCollection) return {
+                errors:
+                {message:"Server Error!!"}
+        };
+
+        const existingUser=await userCollection.findOne({ email })
+        if(existingUser){
+                return{
+                        errors:
+                {message:"Email already exist. Please log in with your email!!"}
+                }
+        }
+        
+        const hashedPassword=await bcrypt.hash(password,10);
+        const results=await userCollection.insertOne({email,password:hashedPassword})
+
+        await createSession(results.insertedId.toString())
+        redirect("/dashboard")
+        
+}
